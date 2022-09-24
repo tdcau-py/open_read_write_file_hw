d9019de25d3bcb3c61d32e6dd3828afdaddedf19
tdcau-py <borisov.p.92@yandex.ru>
2022-09-24 17:39:27 +0300

    Added function get_shop_list_by_dishes

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from pprint import pprint
+from typing import List
+import os
 
 
-def read_file(file: str) -> list:
-    with open(file, encoding='utf-8') as file:
+def read_file(path_to_file: str) -> List[list]:
+    """Читает данные из файла и группирует их в списки"""
+    with open(path_to_file, encoding='utf-8') as file:
         recipes = file.read().split('\n')
 
-    recipes_list = []
+    recipes_lists = []
     while recipes:
         if '' in recipes:
-            recipes_list.append(list(recipes[:recipes.index('')]))
+            recipes_lists.append(list(recipes[:recipes.index('')]))
             del recipes[0:recipes.index('') + 1]
 
         else:
-            recipes_list.append(list(recipes[:]))
+            recipes_lists.append(list(recipes[:]))
             recipes.clear()
 
-    return recipes_list
+    return recipes_lists
 
 
 def get_cook_book() -> dict:
-    recipes_list = read_file('recipes.txt')
+    """Формирует словарь из сгруппированных данных"""
+    file_name = 'recipes.txt'
+    dir_path = os.getcwd()
+    recipes_list = read_file(os.path.join(dir_path, file_name))
     recipes_book = {}
 
     for item in recipes_list:
@@ -34,5 +40,18 @@ def get_cook_book() -> dict:
     return recipes_book
 
 
-cook_book = get_cook_book()
-pprint(cook_book)
+def get_shop_list_by_dishes(dishes: list, persons: int) -> dict:
+    cook_book = get_cook_book()
+    shop_list = {}
+
+    for dish in dishes:
+        if dish in cook_book:
+            for item in cook_book[dish]:
+                shop_list[item['ingredient_name']] = {'measure': item['measure'],
+                                                      'quantity': int(item['quantity']) * persons}
+
+    return shop_list
+
+
+new_shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+pprint(new_shop_list)
